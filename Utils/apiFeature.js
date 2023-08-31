2ce8b1a2592f1238a5d9a85c8785d4d735fb7132
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-31 14:52:50 +0100

    worked on apifeature

---
+++ Utils/apiFeature.js
@@ -0,0 +1,72 @@
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+import {kulkonect_chatAddress, kulkonect_chatAbi} from '../Context/constants';
+import { time } from "console";
+
+export const CheckIfWalletConnected = async () => {
+    try{
+        if(!window.ethereum) return console.log("Install MetaMask");
+
+        const accounts = await window.ethereum.request({
+            method: "eth_accounts",
+        });
+
+        const firstAccount = accounts[0];
+        return firstAccount;
+
+    }catch(error){
+        console.log(error);
+    }
+};
+
+export const connectWallet = async () => {
+    try{
+        if (!window.ethereum) return console.log("Install MetaMask");
+
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        const firstAccount = accounts[0];
+        return firstAccount;
+
+    }catch(error){
+        console.log(error);
+    }
+};
+
+const fetchContract = (signerOrProvider) => new ethers.Contract(kulkonect_chatAbi, kulkonect_chatAddress, signerOrProvider);
+
+export const connectingWithContract = async () =>{
+    try{
+        const web3modal = new Web3Modal();
+        const connection = await web3modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = fetchContract(signer);
+
+        return contract;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const convertTime = (time)=>{
+    const newTime = new Date(time.toNumber());
+
+    const realTime = 
+    newTime.getHours() +
+    "/" +
+    newTime.getMinutes() +
+    "/" +
+    newTime.getSeconds() +
+    " Date:" +
+    newTime.getDay() +
+    "/" +
+    (newTime.getMonth() + 1) +
+    "/" +
+    newTime.getFullYear();
+
+    return realTime;
+
+}
