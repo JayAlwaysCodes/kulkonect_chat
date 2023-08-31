c144eef257da362d300a30dbd4231afafca1d041
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-31 14:21:03 +0100

    worked on the context folder and some of the json files

---
+++ Context/constants.js
@@ -0,0 +1,8 @@
+//0x5fbdb2315678afecb367f032d93f642f64180aa3
+
+import kulkonect_chatJSON from './kulkonect_chat.json'
+
+
+export const kulkonect_chatAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+export const kulkonect_chatAbi = kulkonect_chatJSON.abi;
