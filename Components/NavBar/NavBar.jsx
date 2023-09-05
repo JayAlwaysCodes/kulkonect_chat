ced57258d745be75bfd54ea6c7fd30ff87166709
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-09-05 23:12:06 +0100

    added the asset folder

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, {useContext, useState, useEffect} from "react";
 import  Style  from "./NavBar.module.css";
+import { KulkonectchatContext } from "../../context/kulkonectchatContext";
+import { Model, Error } from "../index";
+
 
 const NavBar = () => {
     return (
